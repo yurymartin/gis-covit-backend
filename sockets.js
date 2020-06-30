e55f230d181b108378371054bc3e3ba622641801
martinyuri123 <yuri_1030@hotmail.com>
2020-06-29 20:26:12 -0500

    change sockets.js

--- sockets.js
+++ sockets.js
@@ -2,8 +2,8 @@ module.exports = io => {
   io.on('connection', (socket) => {
     console.log(`New User connected: ${socket.id}`);
     socket.on('userCoordinates', coords => {
-      // socket.broadcast.emit('newUserCoordinates', coords)
-      socket.emit('newUserCoordinates',coords)
+      socket.broadcast.emit('newUserCoordinates', coords)
+      // socket.emit('newUserCoordinates',coords)
     });
   });
 }
